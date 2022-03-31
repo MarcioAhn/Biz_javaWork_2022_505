8fe4d4229e60764a640826e1ef5c2420eb601fb5
dksgur52@naver.com <dksgur52@naver.com>
2022-03-31 12:43:07 +0900

    0331과제

---
+++ Java_20_Control_01/src/com/callor/method/Method_02.java
@@ -0,0 +1,33 @@
+package com.callor.method;
+
+public class Method_02 {
+	public static void main(String[] args) {
+		
+		int intNum1 = num();
+		double douNum1 = douNum();
+		long longNum1 = longNum();
+		float fNum1 = floatNum();
+		boolean bYes = bYes();
+		String str = nation();
+		
+		System.out.println(intNum1);
+		System.out.println(douNum1);
+		System.out.println(longNum1);
+		System.out.println(fNum1);
+		System.out.println(bYes);
+		System.out.println(str);
+	}
+	public static int num() {
+		return 100;}
+	public static double douNum() {
+		return 100.0;}
+	public static long longNum() {
+		return 1011234;}
+	public static float floatNum() {
+		return 100.123f;}
+	public static boolean bYes() {
+		return true;}
+	public static String nation(){
+		return "대한"+"민국";}
+				
+}
